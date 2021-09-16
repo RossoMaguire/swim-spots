3c7ff1e4dfa35e2d2c3d9742931403e6a933baae
Ross Maguire <ross.rmdesigns@gmail.com>
2021-09-16 19:17:24 +0100

    docs: :pencil2: adding explanation of sort function

--- frontend/src/components/utils/DynamicSort.js
+++ frontend/src/components/utils/DynamicSort.js
@@ -1,13 +1,19 @@
 function dynamicSort(property) {
-  var sortOrder = 1;
+  // I found this function on https://stackoverflow.com/questions/1129216/sort-array-of-objects-by-string-property-value
+
+  // I can explain what is going on after some research and testing with my data
+
+  var sortOrder = 1; // set the original sort order
+
+  // if the property value contains a dash then make the sort order -1
+  // and reassign property with the dash removed
   if (property[0] === "-") {
     sortOrder = -1;
     property = property.substr(1);
   }
+
+  // this gets returned by the .sort method on the array of objects it is called on (filteredSpots in my case) a and b being the objects being compared, one after the other similar to a reduce function
   return function (a, b) {
-    /* next line works with strings and numbers,
-     * and you may want to customize it to your needs
-     */
     var result =
       a[property] < b[property] ? -1 : a[property] > b[property] ? 1 : 0;
     return result * sortOrder;
